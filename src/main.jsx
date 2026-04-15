64f3ec4f30cdb432417e9fc5d062762dc4e6d658
JamshedSifat <mdjamshedsifat@gmail.com>
2026-04-15 20:05:24 +0600

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 import { router } from './Router/Route.jsx'
 import { RouterProvider } from 'react-router'
 import { AuthProvider } from './Auth/Context/AuthContext.jsx'
@@ -8,6 +9,7 @@ import { ServiceProvider } from './Auth/Context/ServiceContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    
     <AuthProvider>
       <ServiceProvider>
         <RouterProvider router={router} />
